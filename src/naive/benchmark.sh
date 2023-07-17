7b168154a930ac902e64d6a46d469db13a67eaa2
Malcolm <malcolm@seyd.ca>
2023-07-16 19:48:17 -0700

    benchmarking

---
+++ src/naive/benchmark.sh
@@ -0,0 +1,10 @@
+ hyperfine \
+  "./sha256_benchmark 1 $1"\
+  "./sha256_benchmark 16 $1"\
+  "./sha256_benchmark 17 $1"\
+  "./sha256_benchmark 55 $1"\
+  "./sha256_benchmark 56 $1"\
+  "./sha256_benchmark 63 $1"\
+  "./sha256_benchmark 64 $1"\
+  "./sha256_bench mark 65 $1"\
+  "./sha256_benchmark 999 $1"\
