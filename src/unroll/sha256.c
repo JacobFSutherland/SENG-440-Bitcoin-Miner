26d0eb867dcf14c617b787b95d39a86c455ba16e
Malcolm <malcolm@seyd.ca>
2023-07-26 23:10:17 -0700

    loop unrolling

--- src/unroll/sha256.c
+++ src/unroll/sha256.c
@@ -12,24 +12,6 @@
 
 #define BLOCK_MAX_LEN (SHA256_BLOCK_LEN - MESSAGE_FOOTER_LEN - 1)
 
-const uint32_t kPrimes[64] = {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,  //
-    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,  //
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,  //
-    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,  //
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,  //
-    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,  //
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,  //
-    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,  //
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,  //
-    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,  //
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,  //
-    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,  //
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,  //
-    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,  //
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,  //
-    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
-
 #define CEILDIV(a, b) (((a) + (b)-1) / (b))
 
 #define write32be(p, v)                       \
@@ -128,14 +110,7 @@ void sha256(const uint8_t* input, size_t input_len, uint8_t* output) {
 #undef BYTE_TO_BINARY_PATTERN
 #endif
 
-    // expand message schedule into 64 32-bit words
-    for (register int i = 16; i < 64; i++) {
-      register uint32_t w2 = w[i - 2];
-      register uint32_t w15 = w[i - 15];
-      w[i] = expand1(w2) + w[i - 7] + expand0(w15) + w[i - 16];
-    }
-
-    // hash message schedule
+    // expand and hash message schedule
     register uint32_t a = H[0];
     register uint32_t b = H[1];
     register uint32_t c = H[2];
@@ -147,18 +122,30 @@ void sha256(const uint8_t* input, size_t input_len, uint8_t* output) {
 
     register uint32_t t1 = 0;
     register uint32_t t2 = 0;
-    for (register int i = 0; i < 64; i++) {
-      t1 = h + hash1(e) + choose(e, f, g) + kPrimes[i] + w[i];
-      t2 = hash0(a) + majority(a, b, c);
-      h = g;
-      g = f;
-      f = e;
-      e = d + t1;
-      d = c;
-      c = b;
-      b = a;
-      a = t1 + t2;
-    }
+
+    register uint32_t w2;
+    register uint32_t w15;
+    register uint32_t wi;
+
+#define EXPAND_ITER(i)                                    \
+  w2 = w[i - 2];                                          \
+  w15 = w[i - 15];                                        \
+  wi = expand1(w2) + w[i - 7] + expand0(w15) + w[i - 16]; \
+  w[i] = wi;
+
+#define HASH_ITER(k)                            \
+  t1 = h + hash1(e) + choose(e, f, g) + k + wi; \
+  t2 = hash0(a) + majority(a, b, c);            \
+  h = g;                                        \
+  g = f;                                        \
+  f = e;                                        \
+  e = d + t1;                                   \
+  d = c;                                        \
+  c = b;                                        \
+  b = a;                                        \
+  a = t1 + t2;
+
+#include "codegen.c"
 
     // update hash state
     H[0] += a;
