ebfdd78d20d21a45264315632e00ca207885b694
Malcolm <malcolm@seyd.ca>
2023-07-27 19:08:42 -0700

    fix benchmarking code for new sha impl

--- src/domain/sha256_benchmark.c
+++ src/domain/sha256_benchmark.c
@@ -22,9 +22,12 @@ int main(int argc, char** argv) {
   for (int i = 0; i < input_len; i++) {
     input[i] = i & 0xFF;
   }
+  size_t padded_input_len;
+  uint8_t* padded_input =
+      sha256_alloc_padded(input, input_len, &padded_input_len);
   for (int i = 0; i < iterations; i++) {
-    sha256(input, input_len, output);
-    input[input_len - 1] = output[0];
+    sha256(padded_input, padded_input_len, output);
+    padded_input[input_len - 1] = output[0];
   }
   free(input);
   return 0;
